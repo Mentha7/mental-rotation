7c15ad5a1acae33a2fb5d46e4e65c5169510aba9
Jingwen <jingwen.li@student.uni-tuebingen.de>
2022-01-18 00:34:48 +0100

    test submission

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,9 +1,9 @@
 export default {
-	experimentId: '171',
+	experimentId: '289',
 	serverUrl: 'https://magpie-demo.herokuapp.com/',
 	// this will be used in prolific mode
 	completionUrl: 'https://...',
 	contactEmail: 'test@random.com',
 	// Either 'debug', 'directLink' or 'prolific'
-	mode: 'debug'
+	mode: 'directLink'
 };
