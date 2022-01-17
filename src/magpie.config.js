2e90e667842804a07557def7e608d2d0e249fd87
Jingwen <jingwen.li@student.uni-tuebingen.de>
2022-01-18 00:44:44 +0100

    Update magpie.config.js

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
 	experimentId: '289',
-	serverUrl: 'https://magpie-demo.herokuapp.com/',
+	serverUrl: 'https://magpie-demo.herokuapp.com/api/submit_experiment/',
 	// this will be used in prolific mode
 	completionUrl: 'https://...',
 	contactEmail: 'test@random.com',
